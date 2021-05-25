495a622859d5c999eefbc01600941ba58b4996fe
Charlos16v <cdominguez@cifpfbmoll.eu>
2021-05-25 09:51:23 +0200

    build: upload content from origin to master

---
+++ src/main/java/edu/pingpong/active/record/domain/Fruit.java
@@ -0,0 +1,41 @@
+package edu.pingpong.active.record.domain;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+
+@JsonPropertyOrder({"name", "description"})
+public class Fruit {
+
+    @NotBlank
+    public String name;
+
+    @NotEmpty
+    public String description;
+
+    // Required constructor by the JSON serialization layer
+    public Fruit() {
+    }
+
+    public Fruit(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ src/main/java/edu/pingpong/active/record/repository/FruitRepository.java
@@ -0,0 +1,46 @@
+package edu.pingpong.active.record.repository;
+
+import edu.pingpong.active.record.domain.Fruit;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Optional;
+import java.util.Set;
+
+// Single bean instance
+@ApplicationScoped
+public class FruitRepository {
+
+    private final Set<Fruit> fruitsData = Collections.newSetFromMap(Collections.synchronizedMap(new LinkedHashMap<>()));
+
+    // Empty constructor for the CDI
+    public FruitRepository() {
+    }
+
+    @PostConstruct
+    public void initDB() {
+        fruitsData.clear(); //Defensive clean of the set
+        fruitsData.add(new Fruit("Strawberry", "Winter fruit"));
+        fruitsData.add(new Fruit("Orange", "Summer fruit"));
+    }
+
+    public Set<Fruit> getData() {
+        return this.fruitsData;
+    }
+
+    public void addFruit(Fruit fruit) {
+        this.fruitsData.add(fruit);
+    }
+
+    public Optional<Fruit> getFruit(String name) {
+        // Optional because it can't always exist, equalsIgnoreCase to check if the string is equals ignoring the case.
+        return getData().stream().filter(fruit -> fruit.getName().equalsIgnoreCase(name)).findFirst();
+    }
+
+    public void removeFruit(String name) {
+        getData().removeIf(fruit -> fruit.getName().equalsIgnoreCase(name));
+        //this.fruitsData.remove(getData().stream().filter(fruit -> fruit.getName().equalsIgnoreCase(name)).findFirst());
+    }
+}
