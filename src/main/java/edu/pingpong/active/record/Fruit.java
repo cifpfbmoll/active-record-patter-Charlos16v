a19c6998629837e178b1d184092548f2c4348964
Charlos16v <cdominguez@cifpfbmoll.eu>
2021-05-26 23:20:05 +0200

    fix: added @Column annotations, @JsonPropertyOrdercorrect and @Table annotation

--- src/main/java/edu/pingpong/active/record/Fruit.java
+++ src/main/java/edu/pingpong/active/record/Fruit.java
@@ -1,24 +1,22 @@
 package edu.pingpong.active.record;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 
 @Entity
-@Table(name="fruits")
-//@JsonPropertyOrder({"name", "description"})
+@Table(name="Fruit")
+@JsonPropertyOrder({"name", "description"})
 public class Fruit extends PanacheEntity {
 
     @NotBlank
-    @Column
     public String name;
 
     @NotEmpty
-    @Column
     public String description;
 
     // Required constructor by the JSON serialization layer
